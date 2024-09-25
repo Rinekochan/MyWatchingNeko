fb6598a1e921bc1c06f3282adec7159ec58eea00
Henry <viethoang9314@gmail.com>
2024-09-25 23:49:25 +1000

    Add Carousel, API

--- src/components/Home/HomeCarousel.tsx
+++ src/components/Home/HomeCarousel.tsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { FaRegThumbsUp } from 'react-icons/fa'
+import { tmdb_path } from '../../utils/constant';
+import { baseApi } from '../../api/axiosInstance';
+
 
 const HomeCarousel = () => {
+    interface CarouselMovies {
+        backdrop_path: string;
+        poster_path: string;
+        title: string;
+        overview: string;
+        vote_count: number;
+    }
+
+    const [carouselMovies, setCarouselMovies] = useState<CarouselMovies[]>([])
+
+    const fetchUpcoming = async () => {
+        try {
+            const response = await baseApi.get('/3/movie/upcoming?language=en-US&page=1')
+            console.log(response.data.results)
+            setCarouselMovies(response.data.results)
+        }
+        catch (err) {
+            console.error("Fetch upcoming movies error", err);
+        }
+    }
+
+    useEffect(() => {
+        fetchUpcoming()
+    }, [])
+
     return (
         <div className='relative'>
             <div className='relative w-[900px]'>
-                <div className="block w-[900px] bg-red-300 aspect-[7/4]"></div>
+                {/* <div className="block w-[900px] bg-red-300 aspect-[7/4]"></div> */}
+                <img src={tmdb_path + carouselMovies[0]?.backdrop_path} className='w-full aspect-[7/4]' alt="" />
                 <div className='absolute bottom-0 h-44 w-full _carouselGradient'></div>
             </div>
             <div className="absolute bottom-0 flex items-end gap-4 px-4">
-                <div className="block bg-blue-400 w-[160px] aspect-[4/5]"></div>
+                <img src={tmdb_path + carouselMovies[0]?.poster_path} className='w-[160px] aspect-[4/5]' alt="" />
                 <div className="flex flex-col gap-1">
-                    <h1 className='text-4xl text-white'>Abra-ca-dabra</h1>
-                    <h2 className='w-[500px] text-xl line-clamp-3 text-zinc-400'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
+                    <h1 className='text-4xl text-white'>{carouselMovies[0]?.title}</h1>
+                    <h2 className='w-[500px] text-xl line-clamp-3 text-zinc-400'>{carouselMovies[0]?.overview}</h2>
                     <div className='flex items-center gap-1 text-zinc-400 text-lg'>
                         <h1><FaRegThumbsUp /> </h1>
-                        <h3 className='text-lg'>457</h3>
+                        <h3 className='text-lg'>{carouselMovies[0]?.vote_count}</h3>
                     </div>
                 </div>
             </div>
---
+++ src/utils/constant.ts
@@ -0,0 +1,1 @@
+export const tmdb_path = "https://image.tmdb.org/t/p/w500"
