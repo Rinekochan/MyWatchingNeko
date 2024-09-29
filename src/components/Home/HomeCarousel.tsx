9e1925ee28f40de88e468bffeb28101288262248
Henry <viethoang9314@gmail.com>
2024-09-29 22:47:49 +1000

    Add Carousel Bootstrap

--- src/components/Home/HomeCarousel.tsx
+++ src/components/Home/HomeCarousel.tsx
@@ -32,22 +32,41 @@ const HomeCarousel = () => {
     }, [])
 
     return (
-        <div className='relative'>
-            <div className='relative w-[900px]'>
-                {/* <div className="block w-[900px] bg-red-300 aspect-[7/4]"></div> */}
-                <img src={tmdb_path + carouselMovies[0]?.backdrop_path} className='w-full aspect-[7/4]' alt="" />
-                <div className='absolute bottom-0 h-44 w-full _carouselGradient'></div>
-            </div>
-            <div className="absolute bottom-0 flex items-end gap-4 px-4">
-                <img src={tmdb_path + carouselMovies[0]?.poster_path} className='w-[160px] aspect-[4/5]' alt="" />
-                <div className="flex flex-col gap-1">
-                    <h1 className='text-4xl text-white'>{carouselMovies[0]?.title}</h1>
-                    <h2 className='w-[500px] text-xl line-clamp-3 text-zinc-400'>{carouselMovies[0]?.overview}</h2>
-                    <div className='flex items-center gap-1 text-zinc-400 text-lg'>
-                        <h1><FaRegThumbsUp /> </h1>
-                        <h3 className='text-lg'>{carouselMovies[0]?.vote_count}</h3>
-                    </div>
+        <div className='relative w-[900px]'>
+            <div id="carousel" className="carousel slide">
+                <div className="carousel-inner">
+                    {
+                        carouselMovies.map((movie, index) => (
+                            <div className={`carousel-item ${index == 0 ? "active" : ""}`}>
+                                <div className='relative '>
+                                    {/* <div className="block w-[900px] bg-red-300 aspect-[7/4]"></div> */}
+                                    <img src={tmdb_path + movie?.backdrop_path} className='w-full aspect-[9/5]' alt="" />
+                                    <div className='absolute bottom-0 h-44 w-full _carouselGradient'></div>
+                                </div>
+                                <div className="absolute bottom-0 flex items-end gap-4 px-4">
+                                    <img src={tmdb_path + movie?.poster_path} className='w-[160px] aspect-[4/5]' alt="" />
+                                    <div className="flex flex-col gap-1">
+                                        <h1 className='text-4xl text-white'>{movie?.title}</h1>
+                                        <h2 className='w-[500px] text-xl line-clamp-3 text-zinc-400'>{movie?.overview}</h2>
+                                        <div className='flex items-center gap-1 text-zinc-400 text-lg'>
+                                            <h1><FaRegThumbsUp /> </h1>
+                                            <h3 className='text-lg'>{movie?.vote_count}</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+
             </div>
         </div>
     )
